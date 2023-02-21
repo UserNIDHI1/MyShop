5c87cff50f3f3fe6e4b0a709ad3d243df948bcad
UserNIDHI1 <nhirpara@aakashinformatics.com>
2023-02-21 13:12:56 +0530

    merge into dependencyInjection to main branch

--- MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T:BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
@@ -38,7 +39,7 @@ namespace MyShop.DataAccess.InMemory
         public void Update(T t)
         {
             T tToUpdate = items.Find(i => i.Id == t.Id);
-            if(tToUpdate!=null)
+            if (tToUpdate != null)
             {
                 tToUpdate = t;
             }
@@ -51,7 +52,7 @@ namespace MyShop.DataAccess.InMemory
         public T Find(string Id)
         {
             T t = items.Find(i => i.Id == Id);
-            if(t!=null)
+            if (t != null)
             {
                 return t;
             }
@@ -66,7 +67,7 @@ namespace MyShop.DataAccess.InMemory
             return items.AsQueryable();
         }
 
-         
+
         public void Delete(string Id)
         {
             T tToDelete = items.Find(i => i.Id == Id);
@@ -79,5 +80,5 @@ namespace MyShop.DataAccess.InMemory
                 throw new Exception(className + "Not found");
             }
         }
-    } 
+    }
 }
