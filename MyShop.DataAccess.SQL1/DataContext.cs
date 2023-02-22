32f839000c06b449bef8d7f4196e653c315084e4
UserNIDHI1 <nhirpara@aakashinformatics.com>
2023-02-22 15:04:07 +0530

    Added Basket Model

--- MyShop.DataAccess.SQL1/DataContext.cs
+++ MyShop.DataAccess.SQL1/DataContext.cs
@@ -19,5 +19,7 @@ namespace MyShop.DataAccess.SQL1
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductsCategories { get; set; }
 
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
     }
 }
