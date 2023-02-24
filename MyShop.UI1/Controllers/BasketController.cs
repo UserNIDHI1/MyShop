7c3a86ee3ae9f061a2a339afc9acb9609e9b94a9
UserNIDHI1 <nhirpara@aakashinformatics.com>
2023-02-24 18:11:41 +0530

    linkedcustomer to orders

--- MyShop.UI1/Controllers/BasketController.cs
+++ MyShop.UI1/Controllers/BasketController.cs
@@ -11,13 +11,15 @@ namespace MyShop.UI1.Controllers
     
     public class BasketController : Controller
     {
+        IRepository<Customer> customers;
         IBasketService basketService;
         IOrderService orderService;
 
-        public BasketController(IBasketService BasketService,IOrderService OrderService)
+        public BasketController(IBasketService BasketService,IOrderService OrderService,IRepository<Customer> Customers)
         {
             this.basketService = BasketService;
             this.orderService = OrderService;
+            this.customers = Customers;
         }
 
         // GET: Basket
@@ -47,16 +49,39 @@ namespace MyShop.UI1.Controllers
             return PartialView(basketSummary);
         }
 
+        [Authorize]
         public ActionResult CheckOut()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+
+            if(customer!=null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    City = customer.City,
+                    State = customer.State,
+                    Street = customer.Street,
+                    FirstName = customer.FirstName,
+                    Surname = customer.LastName,
+                    ZipCode = customer.ZipCode
+                };
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+           
         }
 
         [HttpPost]
+        [Authorize]
         public ActionResult CheckOut(Order order)
         {
             var basketItems = basketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
+            order.Email = User.Identity.Name;
 
             //process payment
 
