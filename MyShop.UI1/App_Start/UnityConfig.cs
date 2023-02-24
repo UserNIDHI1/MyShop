63c51782a12c473893b242695c0d8f8c530917a2
UserNIDHI1 <nhirpara@aakashinformatics.com>
2023-02-24 17:22:07 +0530

    Added Order Creation

--- MyShop.UI1/App_Start/UnityConfig.cs
+++ MyShop.UI1/App_Start/UnityConfig.cs
@@ -51,7 +51,12 @@ namespace MyShop.UI1
             container.RegisterType<IRepository<Basket>, SQLRepository<Basket>>();
             container.RegisterType<IRepository<BasketItem>, SQLRepository<BasketItem>>();
             container.RegisterType<IRepository<Customer>, SQLRepository<Customer>>();
-            container.RegisterType<IBasketService,BasketService>(); 
+            container.RegisterType<IRepository<Order>, SQLRepository<Order>>();
+
+            container.RegisterType<IBasketService,BasketService>();
+            container.RegisterType<IOrderService, OrderService>();
+
+
         }
     }
 }
--- MyShop.UI1/Controllers/BasketController.cs
+++ MyShop.UI1/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,12 @@ namespace MyShop.UI1.Controllers
     public class BasketController : Controller
     {
         IBasketService basketService;
+        IOrderService orderService;
 
-        public BasketController(IBasketService BasketService)
+        public BasketController(IBasketService BasketService,IOrderService OrderService)
         {
             this.basketService = BasketService;
+            this.orderService = OrderService;
         }
 
         // GET: Basket
@@ -42,6 +45,32 @@ namespace MyShop.UI1.Controllers
             var basketSummary = basketService.GetBacketSummary(this.HttpContext);
 
             return PartialView(basketSummary);
+        }
+
+        public ActionResult CheckOut()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CheckOut(Order order)
+        {
+            var basketItems = basketService.GetBasketItems(this.HttpContext);
+            order.OrderStatus = "Order Created";
+
+            //process payment
+
+            order.OrderStatus = "Payment Processed";
+            orderService.CreateOrder(order, basketItems);
+            basketService.ClearBasket(this.HttpContext);
+
+            return RedirectToAction("ThankYou",new { OrderId=order.Id});
+        }
+
+        public ActionResult ThankYou(string OrderId)
+        {
+            ViewBag.OrderId = OrderId;
+            return View();
         }
     }
 }
